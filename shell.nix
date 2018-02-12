3f9e1b825e7ae59eef171aff9688a6673b2cd5ce
lucas8 <luc.linux@mailoo.org>
2018-02-12 20:42:14 +0100

    First commit

---
+++ shell.nix
@@ -0,0 +1,32 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, array, base, containers, diagrams, stdenv
+      , transformers, diagrams-cairo
+      }:
+      mkDerivation {
+        pname = "bezier";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [
+          array base containers diagrams diagrams-cairo transformers
+        ];
+        license = stdenv.lib.licenses.mit;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
